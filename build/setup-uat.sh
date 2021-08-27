2fa90ad41fcf3dc40cd66f6a1f7cb1d96bb50d41
Karen Rodríguez <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-08-27 16:47:29 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -7,6 +7,7 @@ sfdx alias:set my-hub-orgDESA=sbravo@nectia.com.sbx2021
 sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
 
 echo "Conectar QA"
+sfdx auth:web:login --setalias my-hub-orgQA --instanceurl https://test.salesforce.com
 sfdx alias:set my-hub-orgQA=administrador_nectia@nectia.com.qa
 sfdx auth:jwt:grant --clientid $SFDC_QA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_QA_USER --instanceurl https://test.salesforce.com
 
