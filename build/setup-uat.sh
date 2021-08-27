fa84d547e6632f00016085549f2774985789f59f
Karen Rodríguez <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-08-27 16:26:26 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -10,10 +10,10 @@ echo "Conectar QA"
 sfdx alias:set my-hub-orgQA=administrador_nectia@nectia.com.qa
 sfdx auth:jwt:grant --clientid $SFDC_QA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_QA_USER --instanceurl https://test.salesforce.com
 
-echo "Conectar PreProd"
-sfdx alias:set my-hub-orgUAT2=administrador_nectia@nectia.com.preprod
-sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
+#echo "Conectar PreProd"
+#sfdx alias:set my-hub-orgUAT2=administrador_nectia@nectia.com.preprod
+#sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
 
-echo "Conectar Prod"
-sfdx alias:set my-hub-Prod=administrador_nectia@nectia.com
-sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --instanceurl https://login.salesforce.com
+#echo "Conectar Prod"
+#sfdx alias:set my-hub-Prod=administrador_nectia@nectia.com
+#sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --instanceurl https://login.salesforce.com
