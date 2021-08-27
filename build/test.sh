baa8dd52ea678728c89172da0a2b8606102d67ea
Karen Rodríguez <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-08-27 16:33:03 -0400

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,6 +1,6 @@
 echo "Conectar Oxiquim"
 sfdx force:org:list
-sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
+sfdx force:mdapi:convert -d deploy_prod -r pruebaPackage
 
 #Para el paso a QA
 sfdx config:set defaultusername=administrador_nectia@nectia.com.qa
